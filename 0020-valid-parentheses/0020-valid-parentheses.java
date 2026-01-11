aeba9ea705bde36e670d46bcf7656d19251ef3b1
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-01-11 18:02:46 +0530

    Time: 130 ms (6.63%), Space: 46.9 MB (5.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        
-        while(true){
-            if(s.contains("()")){
-                s=s.replace("()","");
-            }else if(s.contains("[]")){
-                s=s.replace("[]","");
-            }else if(s.contains("{}")){
-                s=s.replace("{}","");
-            }
-            else{
+
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } 
+            else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } 
+            else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } 
+            else {
                 return s.isEmpty();
             }
         }
-
-         
     }
 }
