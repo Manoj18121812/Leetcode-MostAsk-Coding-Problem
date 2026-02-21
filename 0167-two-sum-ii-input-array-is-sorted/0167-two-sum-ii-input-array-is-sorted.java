6ece65f2abaf8cf63f2e4968f4743a1bda9b8e5d
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-02-21 23:00:37 +0530

    Time: 410 ms (5.38%), Space: 48.3 MB (80.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i=0;i<numbers.length;i++){
             for(int j=i+1;j<numbers.length;j++){
                 if(numbers[i]+numbers[j]==target){
-                    return new int[] {i+1,j+1};
+                    return new int[]{i+1,j+1};
                 }
             }
         }
-        return new int []{};
+        return new int[]{};
     }
 }
