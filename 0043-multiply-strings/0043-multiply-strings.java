5b3669ed3d76f6d7b83e7b6961e9b59dd70d3fc0
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-03-06 21:51:12 +0530

    Time: 3 ms (83%), Space: 43.4 MB (96.68%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        /*
+        int num3 = Integer.parseInt(num1);
+        int num4 = Integer.parseInt(num2);
+        double result =num3*num4;
+
+        return  String.valueOf(result);*/
+
+        int m = num1.length();
+        int n = num2.length();
+        int arr[] = new int[m+n];
+
+        for(int i=m-1;i>=0;i--){
+            int d1= num1.charAt(i)-'0';;
+            for(int j =n-1;j>=0;j--){
+                int d2=num2.charAt(j)-'0';
+
+            int mul= d1 * d2;//18
+
+            int sum = mul + arr[i+j+1];//18
+            arr[i+j+1] = sum%10;//8
+            arr[i+j] = arr[i+j]+sum/10;//1
+
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for(int r:arr){
+            if(sb.length()==0 & r==0) continue;
+            sb.append(r);
+        }
+
+        return sb.length()==0 ? "0" :sb.toString();
+
+    }
+}
