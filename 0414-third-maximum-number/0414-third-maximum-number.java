4b3c124992bf1280cae0ab588237bfa9af7673d1
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-03-24 20:36:49 +0530

    Time: 8 ms (19.04%), Space: 46.5 MB (14.46%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int thirdMax(int[] nums) {
         
-        Set<Integer> set = new HashSet<>();
-        
-        for(int num : nums){
-            set.add(num);
-        }
-        
-        if(set.size() < 3){
-            return Collections.max(set);
-        }
-        
-        set.remove(Collections.max(set));
-        set.remove(Collections.max(set));
-        
+       Set<Integer>set = new HashSet<>();
+
+       for(int num:nums){
+        set.add(num);
+       }
+
+       if(set.size()<3){
         return Collections.max(set);
+       }
+
+       set.remove(Collections.max(set));
+       set.remove(Collections.max(set));
+
+       return Collections.max(set);
     }
 }
