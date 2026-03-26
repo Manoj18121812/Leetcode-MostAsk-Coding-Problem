0cbd81cc00ad5b49a7bd43ecd8ddefa7c522e33d
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-03-26 14:48:10 +0530

    Time: 18 ms (36.15%), Space: 45.2 MB (35.31%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -3,28 +3,31 @@ class Solution {
         
 
         paragraph =paragraph.toLowerCase().replaceAll("[^a-z]"," ");
-        String words[]= paragraph.split(" ");
 
-        HashSet<String>ban= new HashSet<>();
-            for(String b:banned){
-                ban.add(b);
+        String words[]=paragraph.split(" ");
+
+        Set<String>set = new HashSet<>();
+
+        for(String b:banned){
+            set.add(b);
+        }
+
+        HashMap<String,Integer>map = new HashMap<>();
+
+        int max=0;
+        String result="";
+
+        for(String word:words){
+
+            if(word.length()==0 || set.contains(word)) continue;
+
+            map.put(word,map.getOrDefault(word,0)+1);
+
+            if(map.get(word)>max){
+                max= map.get(word);
+                result = word;
             }
-
-            HashMap<String,Integer>map= new HashMap<>();
-            String result="";
-            int max=0;
-
-            for(String word:words){
-                if(word.length()==0 || ban.contains(word)) continue;
-
-                map.put(word,map.getOrDefault(word,0)+1);
-
-                    if(map.get(word)>max){
-                        max= map.get(word);
-                        result =word;
-                    }
-
-            }
-            return result;
+        }
+        return result;
     }
 }
