cedacea0c7960de54a9fd135a3a3bcbfc4326fd9
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-03-27 16:27:47 +0530

    Time: 1 ms (99.61%), Space: 45.1 MB (82.8%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-
         int n = prices.length;
-   
-     int res[]= Arrays.copyOf(prices,n);
-     Stack<Integer> stack = new Stack<>();
 
-     for(int i=0;i<prices.length;i++){
-        while(!stack.isEmpty() && prices[stack.peek()] >= prices[i]){
-            int idx = stack.pop();
-            res[idx] = res[idx] -prices[i];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
         }
-        stack.push(i);
-     }
-    return res;
+
+        return prices;
     }
 }
