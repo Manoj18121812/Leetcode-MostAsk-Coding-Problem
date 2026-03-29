06659fd31fce1e310cce3f3418b11906005a2f97
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-03-29 17:53:26 +0530

    Time: 272 ms (15.91%), Space: 47.1 MB (6.1%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,19 +1,17 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         
-        String s= a;
+        String s=a;
         int count=1;
 
         while(s.length()<b.length()){
             s=s+a;
             count++;
         }
-
         if(s.contains(b)) return count;
 
         s=s+a;
         count++;
-
         if(s.contains(b)) return count;
 
         return -1;
