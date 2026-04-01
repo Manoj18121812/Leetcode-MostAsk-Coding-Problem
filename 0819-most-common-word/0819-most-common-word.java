1c80353703d1a770419e1e2a966b7a77921a88be
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-01 16:10:39 +0530

    Time: 14 ms (69.83%), Space: 45.3 MB (26.03%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,30 +1,25 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        
 
-        paragraph =paragraph.toLowerCase().replaceAll("[^a-z]"," ");
+        paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
+        String words[] = paragraph.split(" ");
 
-        String words[]=paragraph.split(" ");
-
-        Set<String>set = new HashSet<>();
-
-        for(String b:banned){
-            set.add(b);
+        Set<String> set = new HashSet<>();
+        for (String ch : banned) {
+            set.add(ch);
         }
 
-        HashMap<String,Integer>map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        int max = 0;
+        String result = "";
 
-        int max=0;
-        String result="";
+        for (String word : words) {
+            if (word.length() == 0 || set.contains(word)) continue;
 
-        for(String word:words){
+            map.put(word, map.getOrDefault(word, 0) + 1);
 
-            if(word.length()==0 || set.contains(word)) continue;
-
-            map.put(word,map.getOrDefault(word,0)+1);
-
-            if(map.get(word)>max){
-                max= map.get(word);
+            if (map.get(word) > max) {
+                max = map.get(word);
                 result = word;
             }
         }
