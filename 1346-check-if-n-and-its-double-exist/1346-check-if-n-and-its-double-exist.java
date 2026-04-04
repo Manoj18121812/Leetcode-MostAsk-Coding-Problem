9a36c166d7cbc4ad2c993164da6113d4be92698f
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-04 17:40:13 +0530

    Time: 2 ms (97.73%), Space: 44.3 MB (93.59%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -3,9 +3,10 @@ class Solution {
         
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                if(i!=j && arr[i]==2*arr[j]){
+                if(i!=j && arr[i]==arr[j]*2){
                     return true;
                 }
+
             }
         }
         return false;
