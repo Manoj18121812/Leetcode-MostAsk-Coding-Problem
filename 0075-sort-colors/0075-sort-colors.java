dc42146e40839218cac14e23333a06829ff6d359
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-07 19:58:58 +0530

    Time: 2 ms (18.87%), Space: 43.6 MB (50.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length-1;j++){
                 if(nums[j]>nums[j+1]){
-                    int temp=nums[j+1];
+                    int temp = nums[j+1];
                     nums[j+1]=nums[j];
                     nums[j]=temp;
                 }
