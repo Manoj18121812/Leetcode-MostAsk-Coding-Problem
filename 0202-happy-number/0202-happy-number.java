fe4fd562c108e67726c00c6f7193761782de5260
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-13 15:27:46 +0530

    Time: 1 ms (78.76%), Space: 42.5 MB (40.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,28 +3,26 @@ import java.util.*;
 class Solution {
     public boolean isHappy(int n) {
         
-        Set<Integer> set = new HashSet<>();
+        Set<Integer>set  = new HashSet<>();
 
-        while(n != 1) {
-            if(set.contains(n)) {
+        while(n!=1){
+            if(set.contains(n)){
                 return false;
             }
 
             set.add(n);
-            n = nextNumber(n);
+            n= nextNumber(n);
         }
         return true;
     }
+    private int nextNumber(int num){
 
-    private int nextNumber(int n) {
-        int sum = 0;
-
-        while(n > 0) {
-            int digit = n % 10;
-            sum = sum + digit * digit;
-            n = n / 10;
+        int sum =0;
+        while(num>0){
+            int digit=num%10;
+            sum = sum + digit*digit;
+            num=num/10;
         }
-
         return sum;
     }
 }
