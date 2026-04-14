d07e9172a73cc8b32b66d85e1c0ab5502d406385
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-14 16:09:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.94%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        int countL = 0;
+
+        for(char c : s.toCharArray()){
+            if(c == 'A'){
+                countA++;
+                if(countA > 1) return false;
+                countL = 0;
+            }
+            else if(c == 'L'){
+                countL++;
+                if(countL >= 3) return false;
+            }
+            else {
+                countL = 0;
+            }
+        }
+
+        return true;
+    }
+}
