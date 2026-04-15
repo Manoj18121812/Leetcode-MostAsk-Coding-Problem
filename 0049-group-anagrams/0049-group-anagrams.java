d2e102bd71d7746ad22a02d364e6c56b3b53d734
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-15 16:04:07 +0530

    Time: 7 ms (74.37%), Space: 49.9 MB (31.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -12,4 +12,56 @@ class Solution {
         }
         return new ArrayList<>(map.values());
     }
+}/*import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] strs = new String[n];
+
+        System.out.println("Enter strings:");
+        for (int i = 0; i < n; i++) {
+            strs[i] = sc.nextLine();
+        }
+
+        Solution sol = new Solution();
+        sol.groupAnagrams(strs); // directly print
+
+        sc.close();
+    }
 }
+
+class Solution {
+
+    public void groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(word);
+        }
+
+        // Print result
+        System.out.println("\nGrouped Anagrams:");
+        for (List<String> group : map.values()) {
+            System.out.println(group);
+        }
+    }
+} */
