57d7914b6d0f465424ea59dcba5ff63f96a41e8c
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-15 15:42:51 +0530

    Time: 1 ms (82.9%), Space: 43 MB (8.92%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,26 +1,34 @@
+import java.util.*;
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
 
-       String words[]=s.split(" ");
-       if(pattern.length()!=words.length) return false;
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) return false;
 
-       HashMap<Character,String>map = new HashMap<>();
-        Set<String>set = new HashSet<>();
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
 
-        for(int i=0;i<pattern.length();i++){
+        for (int i = 0; i < pattern.length(); i++) {
+
             char ch = pattern.charAt(i);
-            String word=words[i];
+            String word = words[i];
 
-            if(map.containsKey(ch)){
-                if(!map.get(ch).equals(word)) return false;
-            }else{
-                if(set.contains(word)){
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(word)) {
                     return false;
                 }
-                map.put(ch,word);
+            } else {
+                
+                if (set.contains(word)) {
+                    return false;
+                }
+
+                map.put(ch, word);
                 set.add(word);
             }
         }
+
         return true;
     }
 }
