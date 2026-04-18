cf384be99f81087e8e71026e37bf501ebe93671a
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-18 18:14:34 +0530

    Time: 2087 ms (47.13%), Space: 42.2 MB (49.53%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
 
-       int sum =0;
-
+       int sum=0;
        for(int i=1;i<=num/2;i++){
         if(num%i==0){
-            sum=sum +i;
+            sum=sum+i;
         }
-       } 
+       }
        return sum==num;
     }
 }
