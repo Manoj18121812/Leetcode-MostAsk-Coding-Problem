53f9e8ad680c49e607be55e0e80f0669a34443a6
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-18 16:42:14 +0530

    Time: 1 ms (98.08%), Space: 42.9 MB (13.12%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,13 +1,15 @@
 class Solution {
     public int addDigits(int num) {
-        if (num < 10) return num;
+       while(num>=10){
+        int sum =0;
 
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
+        while(num>0){
+            int digit=num%10;
+            sum = sum + digit;
+            num=num/10;
         }
-
-        return addDigits(sum);
+        num=sum;
+       }
+       return num;
     }
 }
