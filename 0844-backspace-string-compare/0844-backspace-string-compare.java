cb8f531e2aa19834917bbfaba89ca7a771df2b90
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-18 20:27:30 +0530

    Time: 2 ms (57.39%), Space: 42.6 MB (97.54%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -23,4 +23,39 @@ class Solution {
         }
         return sb.toString();
     }
+    }/**import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "ab#c";
+        String t = "ad#c";
+
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        // process s
+        for (char ch : s.toCharArray()) {
+            if (ch != '#') {
+                stack1.push(ch);
+            } else if (!stack1.isEmpty()) {
+                stack1.pop();
+            }
+        }
+
+        // process t
+        for (char ch : t.toCharArray()) {
+            if (ch != '#') {
+                stack2.push(ch);
+            } else if (!stack2.isEmpty()) {
+                stack2.pop();
+            }
+        }
+
+        // compare
+        boolean result = stack1.equals(stack2);
+
+        System.out.println(result);
     }
+} */
