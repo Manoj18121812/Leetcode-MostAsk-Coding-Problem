90ac77ec0d5839f5b12ea1c026dee8291400bdcf
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-19 10:46:22 +0530

    Time: 0 ms (100%), Space: 42.4 MB (63.95%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -3,11 +3,11 @@ class Solution {
 
         StringBuilder sb= new StringBuilder();
 
-        while(col>0){
-            int r=(col-1)%26;
-            sb.append((char)(r+'A'));
-            col=(col-1)/26;
-        }
-        return sb.reverse().toString();
+       while(col>0){
+        int r = (col-1)%26;
+        sb.append((char)(r+'A'));
+        col=(col-1)/26;
+       }
+       return sb.reverse().toString();
     }
 }
